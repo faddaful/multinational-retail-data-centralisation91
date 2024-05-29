40033d115509a53baea9c3000dae26aa98bfd6f4
fadda <olayanjubiodun@gmail.com>
2024-05-29 17:39:36 +0100

    Modified data extraction file ti extract data from the API

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from database_utils import DatabaseConnector
 import tabula
+import requests
 
 class DataExtractor:
     def __init__(self) -> None:
@@ -23,6 +24,57 @@ class DataExtractor:
     def list_db_tables(self, connector):
         return connector.list_db_tables()
     
+    
+    
+    def list_number_of_stores(self, endpoint, headers):
+        response = requests.get(endpoint, headers=headers)
+        if response.status_code == 200:
+            return response.json()['number_stores']
+        else:
+            response.raise_for_status()
+    
+
+    def retrieve_stores_data(self, number_stores_endpoint, store_details_endpoint, headers):
+        #number_of_stores = self.list_number_of_stores(number_stores_endpoint, headers)
+        number_of_stores = extractor.list_number_of_stores(number_stores_endpoint, headers)
+        all_stores_data = []
+
+    
+        for store_number in range(1, number_of_stores + 1):
+            response = requests.get(store_details_endpoint.format(store_number=store_number), headers=headers)
+            if response.status_code == 200:
+                store_data = response.json()
+                all_stores_data.append(store_data)
+            else:
+                response.raise_for_status()
+
+        stores_df = pd.DataFrame(all_stores_data)
+        return stores_df
+    
+# Test to see if the number of stores will be printed
+if __name__ == "__main__":
+    
+    extractor = DataExtractor()
+    headers = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+    number_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
+  
+
+    try:
+        store_number = extractor.list_number_of_stores(number_stores_endpoint, headers)
+        print(store_number)
+    except Exception as e:
+        print('Error retrieving the number of stores: {e}')
+    
+    store_details_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}"
+   
+    try:
+        stores_df = extractor.retrieve_stores_data(number_stores_endpoint, store_details_endpoint, headers)
+        print("Stores data extracted successfully.")
+        print(stores_df.head())  # Display the first few rows of the DataFrame
+    except Exception as e:
+        print(f"Error retrieving stores data: {e}")
+
+
 
 # Usage and testing the pdf extraction
 # if __name__ == "__main__":
